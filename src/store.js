1410ade151734b993c922d7e9522d27de67a0775
a <48353128+zrk@users.noreply.github.com>
2019-04-12 10:26:03 +0300

    add Vuex

---
+++ src/store.js
@@ -0,0 +1,25 @@
+import Vue from 'vue';
+import Vuex from 'vuex';
+
+/* eslint
+   no-shadow: ["error", { "allow": ["state", "getters"] }],
+   no-param-reassign: ["error", { "props": true, "ignorePropertyModificationsFor": ["state"] }]
+*/
+
+Vue.use(Vuex);
+
+export default new Vuex.Store({
+  strict: process.env.NODE_ENV !== 'production',
+
+  state: {
+
+  },
+
+  mutations: {
+
+  },
+
+  actions: {
+
+  },
+});
