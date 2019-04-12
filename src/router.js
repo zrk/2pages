a5f92e3a4bf43a8708b362ffeeef3ff562b358fe
a <48353128+zrk@users.noreply.github.com>
2019-04-12 22:05:24 +0300

    remove router's "history" mode (for gh-pages to work)

--- src/router.js
+++ src/router.js
@@ -5,7 +5,6 @@ import LoginView from './views/LoginView.vue';
 Vue.use(Router);
 
 export default new Router({
-  mode: 'history',
   base: process.env.NODE_ENV === 'production' ? '/2pages/' : '/',
   routes: [
     {
